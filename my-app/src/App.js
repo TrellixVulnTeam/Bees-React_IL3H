a2a8040ed53b1cb47c2c356d44c7b62cbaf1fa1b
Mickie4 <40367446+Mickie4@users.noreply.github.com>
2021-05-05 02:40:17 +0100

    working out the logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,18 @@
 import "./sass/App.scss";
 import Bees from "./components/Bee";
-import AttackBtn from "./components/AttackBtn";
-import workerImage from "./images/worker.png";
-import droneImage from "./images/drone.png";
-import queenImage from "./images/queen.png";
+import beeImg from "./images/bee.png";
 
 function App() {
   return (
     <div className='App'>
-      <Bees image={workerImage} name='Workers' minHealth={70} />
-      <Bees image={droneImage} name='Drones' minHealth={50} />
-      <Bees image={queenImage} name='The Queen' minHealth={20} />
-      <AttackBtn />
+      <Bees
+        image={beeImg}
+        name='Workers'
+        minHealth={70}
+        attackPlaceHolder={0}
+      />
+      <Bees image={beeImg} name='Drones' minHealth={50} />
+      <Bees image={beeImg} name='The Queen' minHealth={20} />
     </div>
   );
 }
--- my-app/src/components/Bee.js
+++ my-app/src/components/Bee.js
@@ -2,17 +2,55 @@ import React, { useState } from "react";
 import "../sass/Bee.scss";
 
 export default function Bee(props) {
+  const [health, setHealth] = useState(100);
+  const [healthIcon, setHealthIcon] = useState("❤️");
+  const [attackNumber, setAttackNumber] = useState("");
+
+  function handleResponse(attackNumber) {
+    if (health && props.name === "Workers") {
+      let score = health - attackNumber;
+      score >= 70 ? setHealth(score) : setHealth(health);
+    }
+
+    // if (health <= 0) {
+    //   console.log("you won!");
+    // if (newScore <= 0) {
+    //   setHealth(0);
+    //   setHealthIcon("💔");
+    // }
+    // newScore <= 0 ? setHealth(0) : setHealth(newScore);
+  }
+
+  function Reset(event) {
+    event.preventDefault();
+    setHealth("");
+    setAttackNumber("");
+    setHealthIcon("❤️");
+  }
+
+  function Attack(event) {
+    event.preventDefault();
+    setAttackNumber(Math.trunc(Math.random() * 100) + 1);
+    handleResponse(attackNumber);
+  }
   return (
-    <div className='worker'>
-      <img src={props.image} alt='little bee' className='logo' />
-      <h1>health %</h1>
-      <h2>Attack power: </h2>
+    <div className='Bee'>
+      <h1 className='Bee__title'>{props.name}</h1>
+      <div className='Bee__img-wrapper'>
+        <img src={props.image} alt='little bee' className='Bee__img-logo' />
+      </div>
+      <h1 className='Bee__header'>
+        {healthIcon} {health} %
+      </h1>
+      <h2 className='Bee__subheader'>💥 {attackNumber}% </h2>
       <ul>
         <li>
           {props.name} need their health to stay above {props.minHealth}%!
         </li>
         <li>Alive status: {}</li>
       </ul>
+      <button onClick={Attack}>Attack!</button>
+      <button onClick={Reset}>Reset</button>
     </div>
   );
 }
